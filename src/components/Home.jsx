4ed6d40a361550b9df378b4c93aba1e3e9a10991
Darby Clement <darbyclement@berkeley.edu>
2021-07-12 22:05:55 -0700

    message

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,4 +1,4 @@
-import { Button, Typography, Box } from '@material-ui/core';
+import { Button, Typography, Box, Paper } from '@material-ui/core';
 import {
     BrowserRouter as Router,
     Switch,
@@ -9,6 +9,7 @@ import {
   } from "react-router-dom";
 import { spacing } from '@material-ui/system';
 
+
 function Home() {
     return(
     <div className="App">
@@ -17,11 +18,18 @@ function Home() {
             How Many Fruits Do I Weigh?
         </Typography>
       </Box>
-        <Button variant="outlined" color="primary" component={Link} to="/my-app/calculator">
-          <Typography variant = 'h3'>
-          Get Started
-          </Typography>
-        </Button>
+    <Box m = {10}>
+    <Typography variant = 'h4'> 
+        Hey. Have you ever looked at a watermelon/coconut/apple and thought 
+        <Box m = {5} style={{fontStyle: 'italic'}}>Hey, how many of those can I put into my body?"</Box>
+        Then this is the site for you!
+    </Typography>
+    </Box>
+    <Button variant="outlined" color="primary" component={Link} to="/my-app/calculator">
+        <Typography variant = 'h3'>
+        Get Started
+        </Typography>
+    </Button>
     </div>
 
     );
